969078f708b82a2092fa1d62b1f61cacfb796638
Kari Bergstedt <kariberg@uw.edu>
2021-02-01 09:52:35 -0800

    Assignment 4 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import './App.css';
-
+import TextInput from './TextInput';
 function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <div className="logo" />
-        CHATTER
-      </header>  
+  const [messages,setMessages] = useState([{text:'hello'}])
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+    <div className="arrow-left"></div>
+    <div className="message">
+      {messages[0].text}
     </div>
-  );
+    <TextInput 
+      send={(t)=> setMessages([{text:t}])}
+    />
+  </div>
 }
-
 export default App;
